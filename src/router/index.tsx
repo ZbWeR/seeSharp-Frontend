c697f8f43ce768a2a94844f6a83d22a0a0258f1b
zbwer <1641064392@qq.com>
2024-04-04 01:13:23 +0800

    🦄 refactor: 移除用于测试路由功能的示例页面

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,6 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import Home from "@/pages/Home";
 import ErrorPage from "@/pages/404";
-import About from "@/pages/About";
 import Shop from "@/pages/Shop";
 import Login from "@/pages/Login";
 
@@ -10,10 +9,6 @@ const routes: RouteObject[] = [
     path: "/",
     element: <Home />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/about",
-    element: <About />,
   },
   {
     path: "/shop",
